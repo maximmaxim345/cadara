75d1e233853fd5de7ff97a2722b9aa323ea56730
Maxim Razantovski <nda.mr43@gmail.com>
2024-03-10 17:04:42 +0100

    chore(utils): add crate

---
+++ crates/utils/src/lib.rs
@@ -0,0 +1,6 @@
+//! Various utilities used throughout the project.
+#![warn(clippy::nursery)]
+#![warn(clippy::pedantic)]
+#![allow(dead_code)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::cognitive_complexity)]
