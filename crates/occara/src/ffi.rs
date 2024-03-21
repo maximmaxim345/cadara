782ddfc69ff4394b03818b664b72438fe3517ea5
Maxim Razantovski <nda.mr43@gmail.com>
2024-03-21 18:29:55 +0100

    refactor: remove unused bindings

--- crates/occara/src/ffi.rs
+++ crates/occara/src/ffi.rs
@@ -9,31 +9,13 @@
 // Run 'touch cpp' and 'bear -- cargo build' in this crates directory for autocompleation of c++ source files
 
 autocxx::include_cpp! {
-    #include "gp_Pnt.hxx"
-    #include "gp_Dir.hxx"
-    #include "gp_Ax1.hxx"
-    #include "MakeBottle.hpp"
     #include "shape.hpp"
     #include "geom.hpp"
+    #include "MakeBottle.hpp"
     safety!(unsafe)
     generate_ns!("occara")
-    generate!("gp_Pnt")
-    generate!("gp_Dir")
-    generate!("gp_Ax1")
     generate!("MakeBottle")
-}
-
-#[cxx::bridge]
-mod ffi2 {
-    unsafe extern "C++" {
-        include!("TopoDS_Shape.hxx");
-        include!("MakeBottle.hpp");
-        type TopoDS_Shape;
-
-        fn MakeBottle(width: f64, height: f64, thickness: f64) -> UniquePtr<TopoDS_Shape>;
-    }
 }
 
 // Re-export the generated bindings
 pub(crate) use ffi::*;
-pub(crate) use ffi2::*;
