4bd5ac915a7c575e7404a85eaf589fd4487b99f1
Maxim Razantovski <nda.mr43@gmail.com>
2024-03-22 15:29:12 +0100

    chore: remove unecessary test

--- crates/occara/src/lib.rs
+++ crates/occara/src/lib.rs
@@ -20,33 +20,3 @@ pub mod internal {
         crate::shape::Shape(crate::ffi::MakeBottle(width, height, thickness).within_box())
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::shape;
-
-    #[test]
-    fn test_vertex_coordinates() {
-        let mut vertex = shape::Vertex::new();
-        let (initial_x, initial_y, initial_z) = vertex.get_coordinates();
-
-        // Check that the initial coordinates are as expected (e.g., (0.0, 0.0, 0.0) if that's the default)
-        assert_eq!(initial_x, 0.0);
-        assert_eq!(initial_y, 0.0);
-        assert_eq!(initial_z, 0.0);
-
-        // Set new coordinates
-        let new_x = 1.0;
-        let new_y = 2.0;
-        let new_z = 3.0;
-        vertex.set_coordinates(new_x, new_y, new_z);
-
-        // Retrieve the coordinates after setting them
-        let (updated_x, updated_y, updated_z) = vertex.get_coordinates();
-
-        // Check that the coordinates have been updated correctly
-        assert_eq!(updated_x, new_x);
-        assert_eq!(updated_y, new_y);
-        assert_eq!(updated_z, new_z);
-    }
-}
