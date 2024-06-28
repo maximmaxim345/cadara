a9677cfda2070f6d52e459b8d008ca9a29e0b331
Maxim Raznatovski <nda.mr43@gmail.com>
2024-06-28 19:13:06 +0200

    feat(viewport): implement ViewportPlugin mechanism
    This commit adds the ability to register ViewportPlugins, that will
    later allow workspaces to completely customize rendering behaviour by
    defining the iced::widget::shader::Primitive responsible for drawing the
    viewport widget.

---
+++ crates/viewport/tests/pipeline/common.rs
@@ -0,0 +1,105 @@
+use computegraph::{node, ComputeGraph};
+use iced::widget::shader::Primitive;
+use viewport::{SceneGraph, ViewportPipeline};
+
+#[derive(Debug)]
+pub struct SomePrimitive();
+impl Primitive for SomePrimitive {
+    fn prepare(
+        &self,
+        _format: wgpu::TextureFormat,
+        _device: &wgpu::Device,
+        _queue: &wgpu::Queue,
+        _bounds: iced::Rectangle,
+        _target_size: iced::Size<u32>,
+        _scale_factor: f32,
+        _storage: &mut iced::widget::shader::Storage,
+    ) {
+    }
+
+    fn render(
+        &self,
+        _storage: &iced::widget::shader::Storage,
+        _target: &wgpu::TextureView,
+        _target_size: iced::Size<u32>,
+        _viewport: iced::Rectangle<u32>,
+        _encoder: &mut wgpu::CommandEncoder,
+    ) {
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct RenderNode();
+#[node(RenderNode)]
+fn run(&self) -> Box<dyn Primitive> {
+    Box::new(SomePrimitive())
+}
+
+#[derive(Debug, Clone)]
+pub struct ConstantNode(usize);
+
+#[node(ConstantNode)]
+fn run(&self) -> usize {
+    self.0
+}
+
+#[derive(Debug, Clone)]
+pub struct CounterState {
+    output_node: ConstantNodeHandle,
+    value: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct InitialCounterNode;
+
+#[node(InitialCounterNode -> (scene, output))]
+fn run(&self) -> (SceneGraph, CounterState) {
+    let mut graph = ComputeGraph::default();
+    let output_node = graph
+        .add_node(ConstantNode(1), "output".to_string())
+        .unwrap();
+    let render_node = graph
+        .add_node(RenderNode(), "renderer".to_string())
+        .unwrap();
+    (
+        SceneGraph {
+            graph,
+            primitive: render_node.output(),
+        },
+        CounterState {
+            output_node,
+            value: 1,
+        },
+    )
+}
+
+#[derive(Debug, Clone)]
+pub struct IncrementCounterNode;
+
+#[node(IncrementCounterNode -> (scene, output))]
+fn run(&self, scene: &SceneGraph, input: &CounterState) -> (SceneGraph, CounterState) {
+    // TODO: it should be possible to opt out of caching to not clone everything everytime
+    let mut scene = (*scene).clone();
+    scene.graph.remove_node(input.output_node.clone()).unwrap();
+    let value = input.value + 1;
+    let output_node = scene
+        .graph
+        .add_node(ConstantNode(value), "output".to_string())
+        .unwrap();
+    (scene, CounterState { output_node, value })
+}
+
+/// the number of ViewportPlugins that were executed in the pipelines
+///
+/// For use with [`InitialCounterNode`] and [`IncrementCounterNode`]
+pub fn node_count(pipeline: &ViewportPipeline) -> Result<usize, Box<dyn std::error::Error>> {
+    let g = pipeline.compute_scene()?.graph;
+    let out_port = computegraph::OutputPortUntyped {
+        node: computegraph::NodeHandle {
+            node_name: "output".to_string(),
+        },
+        output_name: "output",
+    }
+    .to_typed();
+    Ok(g.compute(out_port)?)
+}
