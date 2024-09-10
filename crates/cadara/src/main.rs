1795bdaf9e78ac81269156d54f8969a521ef665b
Maxim Raznatovski <nda.mr43@gmail.com>
2024-09-10 11:48:09 +0200

    TEST

--- crates/cadara/src/main.rs
+++ crates/cadara/src/main.rs
@@ -60,6 +60,14 @@ impl App {
 }
 
 fn main() -> iced::Result {
-    env_logger::init();
+    // env_logger::init();
+    // #[cfg(target_arch = "wasm32")]
+    {
+        console_log::init().expect("Initialize logger");
+        std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    }
+
+    // #[cfg(not(target_arch = "wasm32"))]
+    // tracing_subscriber::fmt::init();
     iced::application("CADara", App::update, App::view).run()
 }
