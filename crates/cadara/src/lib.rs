1ad97b3785e6803dcfe4553148504baa4b3738a1
Maxim Raznatovski <nda.mr43@gmail.com>
2025-02-01 22:55:11 +0100

    feat(cadara): add test setup to check if caching system works

--- crates/cadara/src/lib.rs
+++ crates/cadara/src/lib.rs
@@ -3,12 +3,18 @@
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::cognitive_complexity)]
 
+use iced::{time, Subscription};
 use modeling_module::ModelingModule;
 use std::sync::Arc;
 use workspace::Workspace;
 
 struct App {
     viewport: viewport::Viewport,
+    project: project::Project,
+    project_view: Arc<project::ProjectView>,
+    data_uuid: project::DataId,
+    reg: project::ModuleRegistry,
+    tick_counter: u32,
 }
 
 impl Default for App {
@@ -18,7 +24,9 @@ impl Default for App {
 }
 
 #[derive(Debug)]
-enum Message {}
+enum Message {
+    Tick,
+}
 
 impl App {
     fn new() -> Self {
@@ -46,16 +54,61 @@ impl App {
 
         let project_view = Arc::new(project.create_view(&reg).unwrap());
 
-        let mut viewport = viewport::Viewport::new(project_view);
+        let mut viewport = viewport::Viewport::new(project_view.clone());
         let workspace = modeling_workspace::ModelingWorkspace { data_uuid };
         // TODO: this should dynamically select the first fitting plugin
         let plugin = workspace.viewport_plugins()[0].clone();
         viewport.pipeline.add_dynamic_plugin(plugin).unwrap();
-        Self { viewport }
+        Self {
+            viewport,
+            project,
+            project_view,
+            data_uuid,
+            reg,
+            tick_counter: 0,
+        }
     }
 
-    #[expect(clippy::unused_self)] // required by `iced::application`
-    fn update(&mut self, _message: Message) {}
+    #[expect(clippy::needless_pass_by_value, reason = "required by iced")]
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Tick => {
+                let data = self
+                    .project_view
+                    .open_data_by_id::<ModelingModule>(self.data_uuid)
+                    .unwrap();
+
+                let mut cb = project::ChangeBuilder::from(&data);
+                if self.tick_counter > 100 {
+                    data.apply_persistent(
+                        modeling_module::persistent_data::ModelingTransaction::Create(
+                            modeling_module::persistent_data::Create {
+                                before: None,
+                                operation: modeling_module::operation::ModelingOperation::Grow,
+                            },
+                        ),
+                        &mut cb,
+                    );
+
+                    println!("grow");
+                    self.tick_counter = 0;
+                } else {
+                    let data = self
+                        .project_view
+                        .open_data_by_id::<ModelingModule>(self.data_uuid)
+                        .unwrap();
+
+                    let mut cb = project::ChangeBuilder::from(&data);
+
+                    data.apply_session((), &mut cb);
+                }
+                self.project.apply_changes(cb, &self.reg).unwrap();
+                self.project_view = Arc::new(self.project.create_view(&self.reg).unwrap());
+                self.viewport.update(self.project_view.clone());
+                self.tick_counter += 1;
+            }
+        }
+    }
 
     fn view(&self) -> iced::Element<'_, Message> {
         let viewport_shader = iced::widget::shader(&self.viewport)
@@ -63,6 +116,11 @@ impl App {
             .height(iced::Length::Fill);
 
         iced::widget::column!(iced::widget::text("Viewport:"), viewport_shader).into()
+    }
+
+    #[expect(clippy::unused_self)]
+    fn subscription(&self) -> Subscription<Message> {
+        time::every(time::Duration::from_millis(20)).map(|_| Message::Tick)
     }
 }
 
@@ -86,6 +144,7 @@ pub fn run_cadara() {
     tracing_subscriber::fmt::init();
 
     iced::application("CADara", App::update, App::view)
+        .subscription(App::subscription)
         .run()
         .unwrap();
 }
