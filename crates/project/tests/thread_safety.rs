a303837d9b69ea0b7c12eb92ed1a044cb9712799
Maxim Raznatovski <nda.mr43@gmail.com>
2025-02-03 10:48:13 +0100

    test(project): check for send and sync for tracked variants

--- crates/project/tests/thread_safety.rs
+++ crates/project/tests/thread_safety.rs
@@ -16,4 +16,10 @@ fn test_send_sync() {
     assert_send_sync::<ProjectView>();
     assert_send_sync::<DocumentView<'_>>();
     assert_send_sync::<DataView<'_, MinimalTestModule>>();
+
+    assert_send_sync::<CacheValidator>();
+    assert_send_sync::<AccessRecorder>();
+    assert_send_sync::<TrackedProjectView>();
+    assert_send_sync::<TrackedDocumentView<'_>>();
+    assert_send_sync::<TrackedDataView<'_, MinimalTestModule>>();
 }
